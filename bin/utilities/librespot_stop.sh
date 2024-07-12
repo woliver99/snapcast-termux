04fe9ba2fed36d91b658e96da50f5f03c50aced6
woliver99 <oliver@maplenetwork.ca>
2024-07-12 04:56:01 -0400

    Updated ffmpeg kill

--- bin/utilities/librespot_stop.sh
+++ bin/utilities/librespot_stop.sh
@@ -3,6 +3,7 @@
 BASE_DIR="$(dirname "$(dirname "$(dirname "$(realpath "$0")")")")"
 PID_DIR="$BASE_DIR/pid"
 PID_FILE="$PID_DIR/librespot.pid"
+PIPE_DIR="$BASE_DIR/pipe"
 
 # Check if the PID file exists
 if [ -f "$PID_FILE" ]; then
@@ -14,6 +15,10 @@ if [ -f "$PID_FILE" ]; then
     done
     # Remove the PID file
     rm -f "$PID_FILE"
+
+    # Kill the FFmpeg process
+    pkill -9 "ffmpeg -y -f s16le -ar 44100 -ac 2 -i $PIPE_DIR/librespot.pcm -ar 48000 -f s16le $PIPE_DIR/librespot_resampled.pcm" > /dev/null 2>&1
+
     echo "Librespot stopped successfully."
 else
     echo "Librespot is not running."
